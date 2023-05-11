dd6534114687fcab6207a769f46c3298c3d24a4f
mariyatarasenko88 <tmv1231@yandex.ru>
2023-05-11 21:54:45 +0300

    Homework2.12 is donne

---
+++ src/main/java/pro/sky/homework2_12/service/CalculatorService.java
@@ -0,0 +1,11 @@
+package pro.sky.homework2_12.service;
+
+public interface CalculatorService {
+    String printPlus(Integer a, Integer b);
+
+    String printMinus(Integer a, Integer b);
+
+    String printMultiply(Integer a, Integer b);
+
+    String printDivide(Integer a, Integer b);
+}
---
+++ src/test/java/pro/sky/homework2_12/CalculatorServiceImplTestConstants.java
@@ -0,0 +1,8 @@
+package pro.sky.homework2_12;
+
+public class CalculatorServiceImplTestConstants {
+    public static final Integer ZER0 = 0;
+    public static final Integer ONE = 1;
+    public static final Integer FIVE = 5;
+    public static final Integer MAX = Integer.MAX_VALUE;
+}
