6fccaed851c0573a282cd4890ed3ef40af382c3a
mariyatarasenko88 <tmv1231@yandex.ru>
2023-05-11 18:54:41 +0300

    Initial commit

---
+++ src/test/java/pro/sky/homework2_12/ApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.homework2_12;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
